62079089372d45ab5781692a5d4654578b01eabc
zeltovhorton <azeltov@hortonworks.com>
2017-04-21 13:54:04 -0400

    re-organize

--- makecsv.sh
+++
@@ -1,3 +0,0 @@
-fbn=`basename $1`
-echo query_number,rows_selected,query_time
-sed "s/.*query\(.\{1,2\}\)_sql_2017-[0-9]\+-[0-9]\+-[0-9]\+-[0-9]\+:\(.*\) rows* selected (\(.*\) seconds)/\1,\2,\3/" $1 
